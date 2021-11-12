358dc3d72549ac623aa55fcdc0a9c6c625556576
Advent <21107733+AdventGit@users.noreply.github.com>
2021-11-11 22:46:44 -0600

    Yeet

--- 1Link Better DL Landing Page.user.js
+++ 1Link Better DL Landing Page.user.js
@@ -1,13 +1,13 @@
 //==UserScript==
-//@name 1Link Better DL Landing Page
-//@namespace AdventShit
-//@version 1.0
+//@name        1Link Better DL Landing Page
+//@namespace   AdventShit
+//@version     1.0
 //@description Auto-Switches to the better DL landing page.
-//@author Advent
-//@match *://1link.club/*
-//@grant none
+//@author      Advent
+//@match       *://1link.club/*
+//@grant       none
 //@downloadURL https://github.com/AdventGit/Userscripts-Things/raw/main/1Link%20Better%20DL%20Landing%20Page.user.js
-// ==/UserScript==
+//==/UserScript==
 (function(){
   'use strict';
   const curLoc=window.location.href;
